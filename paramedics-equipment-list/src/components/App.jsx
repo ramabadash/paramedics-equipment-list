1e0f64ff3acf2b6ec899f719d2cf31b121d7653e
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-23 12:12:17 +0200

    # Add InventoryList component with table from the full equipment list from the "db"

--- paramedics-equipment-list/src/components/App.jsx
+++ paramedics-equipment-list/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import EntryForm from './EntryForm';
+import InventoryList from './InventoryList';
 
 function App() {
   return (
     <div>
       <EntryForm />
+      <InventoryList />
     </div>
   );
 }
---
+++ paramedics-equipment-list/src/components/InventoryList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { fullEquipmentList } from '../data/db';
+
+function InventoryList() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>#</th>
+          <th>Item's name</th>
+          <th>Full quantity</th>
+          <th>Current quantity</th>
+          <th>Missing</th>
+        </tr>
+        <tbody>
+          {fullEquipmentList.map((item, i) => {
+            return [
+              <tr key={i}>
+                <td>{i}</td>
+                <td>{item.name}</td>
+                <td>{item.fullQuantity}</td>
+                <td>
+                  <input type='number' min={0} />
+                </td>
+                <td>?</td>
+              </tr>,
+            ];
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default InventoryList;
