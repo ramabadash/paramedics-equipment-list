2712721b9a05619c90d6744f06d6c91418d70178
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-23 17:09:19 +0200

    # Save the values of the inputs in the state of the component and by clicking on submit navigation and update the state by dispatch

--- paramedics-equipment-list/src/components/EntryForm.jsx
+++ paramedics-equipment-list/src/components/EntryForm.jsx
@@ -1,30 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { workerLogin } from '../actions/workerActions';
 
 function EntryForm() {
+  /***** STATE *****/
+  const [fullName, setFullName] = useState('');
+  const [date, setDate] = useState('');
+  const [ambulanceNumber, setAmbulanceNumber] = useState('');
+  const [shift, setShift] = useState('morning');
+
+  /***** FUNCTIONS *****/
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (!fullName || !date || !ambulanceNumber || !shift) {
+      // TODO - add error alert
+      console.log('missing details');
+      return;
+    } else {
+      dispatch(workerLogin(fullName, date, ambulanceNumber, shift)); // Save to state
+      navigate('/inventory'); // Go to next form
+    }
+  };
+
   return (
     <div>
       <h2>Entrance to the shift</h2>
       <div>
         <label htmlFor='full-name'>Full name:</label>
-        <input type='text' name='full-name' />
-      </div>
-      <div>
-        <label htmlFor='ambulance-number'>Ambulance number:</label>
-        <input type='text' name='ambulance-number' />
+        <input
+          type='text'
+          name='full-name'
+          onChange={e => setFullName(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor='date'>Date:</label>
-        <input type='date' name='date' />
+        <input
+          type='date'
+          name='date'
+          onChange={e => setDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor='ambulance-number'>Ambulance number:</label>
+        <input
+          type='number'
+          name='ambulance-number'
+          onChange={e => setAmbulanceNumber(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor='shift'>Shift:</label>
-        <select>
+        <select onChange={e => setShift(e.target.value)}>
           <option value='morning'>Morning</option>
           <option value='evening'>Evening</option>
           <option value='night'>Night</option>
         </select>
       </div>
-      <button>Continue to inventory list</button>
+      <button onClick={handleSubmit}>Continue to inventory list</button>
     </div>
   );
 }
