5120d0781be68f421c57fcddbffd980d7fb4aa8a
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-23 11:17:40 +0200

    # Basic react app with redux temlate

---
+++ paramedics-equipment-list/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { store } from './app/store';
+// import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <Provider store={store}>
+      <App />
+    </Provider> */}
+    <h1>HELLO</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
