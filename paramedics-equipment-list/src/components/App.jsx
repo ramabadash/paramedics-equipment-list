96df9f573080f83e25af3017a82425866ed867d5
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-23 14:38:28 +0200

    # Add routers to the forms

--- paramedics-equipment-list/src/components/App.jsx
+++ paramedics-equipment-list/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import EntryForm from './EntryForm';
 import InventoryList from './InventoryList';
+import { Routes, Route } from 'react-router-dom';
 // Style
 import '../styles/App.css';
 
@@ -8,8 +9,10 @@ function App() {
   return (
     <div>
       <h1>Welcome to the shift!</h1>
-      <EntryForm />
-      <InventoryList />
+      <Routes>
+        <Route path='/' element={<EntryForm />} />
+        <Route path='/inventory' element={<InventoryList />} />
+      </Routes>
     </div>
   );
 }
--- paramedics-equipment-list/src/index.js
+++ paramedics-equipment-list/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 // import { store } from './app/store';
 // import { Provider } from 'react-redux';
+// {
+//   /* <Provider store={store}>
+//   <App />
+// </Provider> */
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}>
+    <Router>
       <App />
-    </Provider> */}
-    <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
